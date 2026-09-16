227c81e5f5c16bd84be9b8e22554b0cbab66e8a8
Leonardo Cordero <Leonardocmoreno1903@gmail.com>
2026-09-15 23:59:21 -0500

    Corregir estructura del backend

---
+++ pythonRestApi/ConexionBasededatos.py
@@ -0,0 +1,35 @@
+import os
+import mysql.connector
+from mysql.connector import Error 
+
+class ConexionMysql:
+    def __init__(self):
+        try:
+            self.mibasededatos = mysql.connector.connect(
+
+                host = os.getenv("MYSQLHOST", "localhost"),
+                port = os.getenv("MYSQLPORT", "3306"),
+                user = os.getenv("MYSQLUSER", "root"),
+                password = os.getenv("MYSQLPASSWORD", ""),
+                database = os.getenv("MYSQL_DATABASE", "prueba")
+            )
+
+            self.conexion = self.mibasededatos
+            self.cursor = self.conexion.cursor()
+
+            if self.mibasededatos.is_connected():
+                db_Info = self.mibasededatos.get_server_info()
+                print(f"CONECTADO A MSQL SERVER, VERSION: ", {db_Info})
+        
+        except Error as e:
+            print(f"Error al conectar con MySql: {e}")
+
+#     def cerrar_conexion(self):
+#         if self.mibasededatos.is_connected():
+#             self.cursor.close()
+#             self.conexion.close()
+#             print("CONEXION CERRADA CORRECTAMENTE")
+# if __name__ == "__main__":
+#     conexion = ConexionMysql()
+#     conexion.cerrar_conexion()
+    
---
+++ pythonRestApi/Controladores.py
@@ -0,0 +1,66 @@
+from ModelUsuario import ModelUsuarios
+from flask_login import LoginManager
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'clave-local')
+
+CORS(app)
+usuarios = ModelUsuarios() 
+
+
+@app.route('/login', methods=['POST'])
+def loginUsuario():
+    email = request.json['email']
+    password = request.json['password']
+    resultado = usuarios.verficarUsuario(email, password)
+    return jsonify({"mensaje": resultado})
+
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify(usuarios.obtener_usuarios())
+
+@app.route('/usuario/<int:id>', methods=['GET'])
+def listar_usuario(id):
+    return jsonify(usuarios.obtener_usuario(id))
+
+@app.route('/nuevo_usuario', methods=['POST'])
+def crear_usuario():
+    nombre = request.json['nombre']
+    email = request.json['email']
+    contrasena = request.json['contrasena']
+    usuarios.insertar_usuario(nombre, email, contrasena)
+    return jsonify({"mensaje": "USUARIO CREADO EXITOSAMENTE"})
+
+@app.route('/actualizar_usuario/<int:id>', methods=['PUT'])
+def actualizar_usuario(id):
+    data = request.json
+    nombre = data.get('nombre')
+    email = data.get('email')
+    contrasena = data.get('contrasena')
+    if nombre is None and email is None and contrasena is None:
+        return jsonify({"error": "NO SE PRORPORCIONARON DATOS PARA ACTUALIZAR"}), 400
+    
+    usuario_existente = usuarios.obtener_usuario(id)    
+    if usuario_existente:
+        usuarios.actualizar_usuario(id, nombre, email, contrasena)
+        return jsonify({"mensaje": "USUARIO ACTUALIZADO CORRECTAMENTE"})
+    else:
+        return jsonify({"error": "EL USUARIO NO EXISTE"}), 404
+
+@app.route('/eliminar_usuario/<int:id>', methods=['DELETE'])
+def eliminar_usuario(id):
+    if usuarios.obtener_usuario(id):
+        usuarios.eliminar_usuario(id)
+        return jsonify({"mensaje": "USUARIOS ELIMINADO CORRECTAMENTE"})
+    else:
+        return jsonify({"error": "EL USUARIO NO EXISTE"}), 404
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
